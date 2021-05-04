f5f6f6cfea46909b51e727fe89f118160c02bbba
chicalmeida <fcbalmeida@gmail.com>
2021-05-04 15:51:56 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/proj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj.dir/main.cpp.obj"
+  "libproj.dll.a"
+  "proj.exe"
+  "proj.exe.manifest"
+  "proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
