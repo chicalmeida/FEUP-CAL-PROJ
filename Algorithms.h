6664632f24c16f05b6bbd3d080d2692482a0dbfd
chicalmeida <fcbalmeida@gmail.com>
2021-05-05 09:00:52 +0100

    created file for search algorithms

---
+++ Algorithms.h
@@ -0,0 +1,12 @@
+//
+// Created by Francisca on 05/05/2021.
+//
+
+#ifndef PROJ_ALGORITHMS_H
+#define PROJ_ALGORITHMS_H
+
+void dijksta();
+void depthFirstSearch();
+void aStar();
+
+#endif //PROJ_ALGORITHMS_H
