14675ad2e2d9a0bf95c12f0961051114bc65719d
tomasnr13 <up201906743@fe.up.pt>
2021-05-23 19:05:19 +0100

    merge

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-#include "graphviewer.h"
+#include <graphviewer.h>
+#include "Graph.h"
+#include "Location.h"
 #include "Application.h"
-#include "lib/GraphViewerCpp/SFML/include/SFML/System/Vector2.hpp"
+
+void graphtoview(Graph<Location> graph){
+
+}
+
 
 int main() {
-    Application app = Application();
-    app.start();
-    //GraphViewer gv;
-    //gv.setCenter(sf::Vector2f(300, 300));
-    //gv.createWindow(600, 600);
-    //gv.join();
+    //Application app = Application();
+    //app.start();
+    GraphViewer gv;
+    gv.setCenter(sf::Vector2f(300, 300));
+    gv.createWindow(600, 600);
+    gv.join();
 
     return 0;
 }
